f010a2522ae23bfc700bc33c527cd4a167aeaf0b
Nevadasurfer <jason@kennedyit.com>
2019-11-11 20:48:56 -0700

    ending the game

--- guessing_game.go
+++ guessing_game.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"math/rand"
 )
 
-func main()  {
+func main() {
 	var userInput int
 	var secretNumber int
+
+	rand.Seed(time.Now().Unix())
 	secretNumber = rand.Intn(10)
 	fmt.Println("Secret Number is:", secretNumber)
+
+
 	fmt.Printf("please enter a number: ")
 	fmt.Scan(&userInput)
 	fmt.Println("You entered:", userInput)
-}
+
+	if userInput == secretNumber {
+		fmt.Println("You Won!!")
+	}	else if userInput < secretNumber {
+		fmt.Println("Too Low...")
+	}	else if userInput > secretNumber {
+		fmt.Println("Too High...")
+
+		}
+
+	}
+
 
