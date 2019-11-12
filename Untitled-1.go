c9973cc5a29e72bcb55bcb74f89d361cd215463b
Nevadasurfer <jason@kennedyit.com>
2019-11-11 19:58:59 -0700

    adding my test go stuff

---
+++ Untitled-1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmtPrintln("    `.-::::::-.`   ")
+fmtPrintln(".:-::::::::::::::-:")
+fmtPrintln("`_:::    ::    :::_")
+fmtPrintln(" .:( ^   :: ^   ):.") 
+fmtPrintln(" `:::   (..)   :::.")
+fmtPrintln(" `:::::::UU:::::::`") 
+fmtPrintln(" .::::::::::::::::.") 
+fmtPrintln(" O::::::::::::::::O") 
+fmtPrintln(" -::::::::::::::::-") 
+fmtPrintln(" `::::::::::::::::`")
+fmtPrintln("  .::::::::::::::. ")
+fmtPrintln("    oO:::::::Oo    ")
+}
---
+++ guessing_game.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var userInput int
+	fmt.Println("please enter a number:")
+	fmt.Scan(&userInput)
+	fmt.Println("You entered:", userInput)
+}
