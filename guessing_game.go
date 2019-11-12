3aa8949b99877f52442a7c92b19747d7f56efa1b
Nevadasurfer <jason@kennedyit.com>
2019-11-11 20:40:38 -0700

    adding secret number

--- guessing_game.go
+++ guessing_game.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main()  {
 	var userInput int
-	fmt.Println("please enter a number:")
+	var secretNumber int
+	secretNumber = rand.Intn(10)
+	fmt.Println("Secret Number is:", secretNumber)
+	fmt.Printf("please enter a number: ")
 	fmt.Scan(&userInput)
 	fmt.Println("You entered:", userInput)
 }
+
